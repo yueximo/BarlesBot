0e762967b2a78770316afee4b0bffe26763f7d16
yueximo <yueximo2024@u.northwestern.edu>
2022-04-03 00:54:00 -0500

    youtube play

---
+++ src/commands/play.js
@@ -0,0 +1,17 @@
+module.exports = {
+  name: 'play',
+  aliases: ['p'],
+  description: 'plays a song from youtube',
+  execute(client, message, args, Discord) {
+    const ytdl = require('ytdl-core');
+    const streamOptions = { seek: 0, volume: 1 };
+    const stream = ytdl(args[0], { filter: 'audioonly' });
+    const dispatcher = message.guild.voiceConnection.play(
+      stream,
+      streamOptions
+    );
+    dispatcher.on('end', () => {
+      message.channel.send('Song ended');
+    });
+  },
+};
--- src/index.js
+++ src/index.js
@@ -1,5 +1,71 @@
 require('dotenv').config;
+//add a youtube function to my discord bot
+const ytdl = require('ytdl-core');
+const ytsearch = require('youtube-search');
+const yt = require('ytdl-core');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = '!';
+const token = process.env.TOKEN;
+const queue = new Map();
 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', (msg) => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
+});
+
+client.on('message', (msg) => {
+  if (msg.content === '!help') {
+    msg.reply('!play [url]');
+  }
+});
+
+client.on('message', (msg) => {
+  if (msg.content === '!play') {
+    msg.reply('!play [url]');
+  }
+});
+
+client.on('message', (msg) => {
+  if (msg.content === '!play [url]') {
+    msg.reply('!play [url]');
+  }
+});
+
+client.on('message', (msg) => {
+  if (msg.content === '!play [url]') {
+    msg.reply('!play [url]');
+  }
+});
+
+client.on('message', (msg) => {
+  if (msg.content === '!play [url]') {
+    msg.reply('!play [url]');
+  }
+});
+
+client.on('message', (msg) => {
+  if (msg.content === '!play [url]') {
+    msg.reply('!play [url]');
+  }
+});
+
+client.on('message', (msg) => {
+  if (msg.content === '!play [url]') {
+    msg.reply('!play [url]');
+  }
+});
+
+client.on('message', (msg) => {
+  if (msg.content === '!play [url]') {
+    msg.reply('!play [url]');
+  }
+});
 const Discord = require('discord.js');
 
 const client = new Discord.Client({
